bc0bc3b3fcf95fa8719c303251bdf68d73eff6b5
vukile-coder <72529053+vukile-coder@users.noreply.github.com>
2020-10-11 11:20:04 +0200

    Update put-cpu-load.php

--- put-cpu-load.php
+++ put-cpu-load.php
@@ -2,16 +2,16 @@
   # Start PHP Session to keep track of whether or not load is getting generated
   session_start();
   
-  echo "<meta http-equiv=\"refresh\" content=\"10,URL=\FirstPHPProject\load.php\" />";
+  echo "<meta http-equiv=\"refresh\" content=\"5,URL=\FirstPHPProject\load.php\" />";
 
   $idleCpu = exec('vmstat 1 2 | awk \'{ for (i=1; i<=NF; i++) if ($i=="id") { getline; getline; print $i }}\'');
 
   if ($idleCpu > 50) {
 
     echo exec('dd if=/dev/zero bs=100M count=500 | gzip | gzip -d  > /dev/null &');
-    echo "Generating CPU Load! (auto refresh in 10 seconds)";
+    echo "Generating CPU Load! (auto refresh in 5 seconds)";
   }
   else {
-    echo "Under High CPU Load! (auto refresh in 10 seconds)";
+    echo "Under High CPU Load! (auto refresh in 5 seconds)";
   }
 ?>
